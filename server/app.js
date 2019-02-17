9a9c80800872d89e78adb08c692679dc2320364f
Mikalai Shchakatsikhin <Mikalai_Shchakatsikhin@epam.com>
2019-02-17 21:22:04 +0300

    feture(server): setup winston logger

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const expressWinston = require('express-winston');
+const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, printf } = format;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const newsRouter = require('./routes/news')
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,11 +26,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
+const myFormat = printf(info =>  `${info.timestamp} ${info.message}`);
+
+app.use(expressWinston.logger({
+  format: combine(timestamp(), myFormat),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: 'info.log',
+      level: 'info'
+    })
+  ]
+}));
+
+
+app.use(expressWinston.errorLogger({
+  transports: [ new winston.transports.Console() ],
+  format: combine(timestamp(), myFormat)
+}));
 
 // error handler
 app.use(function(err, req, res, next) {
